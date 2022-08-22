7f2fcd2bddfc2e830f48c625b3a16144f0861167
Vanessa <tauchertvanessa@gmail.com>
2022-08-22 17:28:33 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,13 +11,19 @@ import java.text.SimpleDateFormat;
 public class Program {
     public static void main(String[] args) throws ParseException {
 
-        Department obj = new Department(1, "livros");
-        SimpleDateFormat sf = new SimpleDateFormat("dd/MM/yyyy");
+        //Department obj = new Department(1, "livros");
+        //SimpleDateFormat sf = new SimpleDateFormat("dd/MM/yyyy");
 
-        Seller sl = new Seller(1, "Vanessa", "teste@teste", sf.parse("17/03/1993"), 3000.00, obj);
+        //Seller sl = new Seller(1, "Vanessa", "teste@teste", sf.parse("17/03/1993"), 3000.00, obj);
+
+        //SellerDao sellerDao = DaoFactory.createSellerDao();
+        //System.out.println(obj);
+        //System.out.println(sl);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
-        System.out.println(obj);
-        System.out.println(sl);
+
+        Seller seller = sellerDao.findById(3);
+        System.out.println(seller);
+
     }
 }
